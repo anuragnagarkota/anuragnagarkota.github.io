6c0d25773efbc2734cd213af63689b0047372686
anuragnagarkota <anuragnagarkota@gmail.com>
2015-11-23 14:39:23 +0530

    Create a.php

---
+++ a.php
@@ -0,0 +1,11 @@
+<?php
+$array=array();
+$arr=array();
+$arr['url']="vikas.php";
+$arr['tag']="Click here to Apply ";
+$array['title']='New Message from Resonance';
+$array['msg']='Announcing New Batches for VIKAAS';
+$array['data']=$arr;
+$array['icon']='icon.png';
+echo json_encode($array);
+?>
